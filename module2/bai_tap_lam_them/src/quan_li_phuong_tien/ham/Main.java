171c2bfde152b7d717ac94365b05fb55754ed0d9
minh230998 <minhnguyenduc27121998@gmail.com>
2021-05-05 14:07:47 +0700

    up bai

--- module2/bai_tap_lam_them/src/quan_li_phuong_tien/ham/Main.java
+++
@@ -1,9 +0,0 @@
-package quan_li_phuong_tien.ham;
-
-import quan_li_phuong_tien.menu.MenuPhuongTien;
-
-public class Main {
-    public static void main(String[] args) {
-        MenuPhuongTien.quanLiPhuongTien();
-    }
-}
