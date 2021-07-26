e5cbd261ba50bb68f3985a0016aa1d867ebe6473
minh230998 <minhnguyenduc27121998@gmail.com>
2021-07-26 17:49:15 +0700

    up bai

--- module5/bai5_input_output/baitap/angular-countdown-timer-app/src/app/countdown-timer/countdown-timer.component.ts
+++ module5/bai5_input_output/baitap/angular-countdown-timer-app/src/app/countdown-timer/countdown-timer.component.ts
@@ -15,19 +15,25 @@ export class CountdownTimerComponent implements OnInit, OnChanges, OnDestroy {
   @Output()
   se = new EventEmitter();
   private intervalId = 0;
+  private a: any = 0;
 
 
   ngOnChanges(changes: SimpleChanges) {
     if ('seconds' in changes) {
       let v = changes.seconds.currentValue;
-      v = typeof v === 'undefined' ? 11 : v;
+      v = typeof v === 'undefined' ? 0 : v;
       const vFixed = Number(v);
-      this.seconds = Number.isNaN(vFixed) ? 11 : vFixed;
+      this.seconds = Number.isNaN(vFixed) ? 0 : vFixed;
     }
   }
 
   clearTimer() {
     clearInterval(this.intervalId);
+  }
+
+  resetTime(a: any) {
+    a = this.remainingTime;
+    clearInterval(a);
   }
 
   ngOnInit() {
@@ -39,9 +45,16 @@ export class CountdownTimerComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   start() {
-    this.countDown();
-    this.remainingTime = this.seconds;
-    this.se.emit(this.seconds);
+    if (this.a !== 0) {
+      this.remainingTime = this.a;
+      this.countDown();
+      this.se.emit(this.a);
+    } else {
+      this.countDown();
+      this.remainingTime = this.seconds;
+      this.se.emit(this.seconds);
+    }
+
     if (this.remainingTime <= 0) {
       this.clearTimer();
       this.finish.emit('Do not countdown!');
@@ -49,27 +62,31 @@ export class CountdownTimerComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   stop() {
+
+    this.a = this.remainingTime;
     this.clearTimer();
-    this.message = `Holding at T-${this.remainingTime} seconds`;
+    this.resetTime(this.remainingTime);
+    this.message = `Holding at T-${this.a} seconds`;
+    this.se.emit(this.message);
   }
 
   reset() {
     this.clearTimer();
     this.remainingTime = this.seconds;
     this.message = `Click start button to start the Countdown, ${this.seconds}`;
-    this.se.emit(this.message) ;
+    this.se.emit(this.message);
   }
 
   private countDown() {
-    this.clearTimer();
+    // this.clearTimer();
     this.intervalId = window.setInterval(() => {
-      this.finish.emit(this.remainingTime)
+      this.finish.emit(this.remainingTime);
       this.remainingTime -= 1;
       if (this.remainingTime === 0) {
-        this.finish.emit('Blast off!') ;
+        this.finish.emit('Blast off!');
         this.clearTimer();
       } else {
-        this.finish.emit(`T-${this.remainingTime} seconds and counting`) ;
+        this.finish.emit(`T-${this.remainingTime} seconds and counting`);
       }
     }, 1000);
   }
